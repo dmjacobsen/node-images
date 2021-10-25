3adb8bfa4c7b670c6724efcb81a44cbe1f653448
Jacob Salmela <jacob.salmela@hpe.com>
2021-10-25 15:43:22 -0500

    remote custom cc_ modules since we deploy them with metal-cloud-init
    this removes cc_ntp and cc_timezone, which have been ported over to
    metal-cloud-init, so we no longer need to manually copy these over.
    
    closes #60
    
    Signed-off-by: Jacob Salmela <jacob.salmela@hpe.com>

--- boxes/ncn-common/provisioners/common/csm/cloud-init.sh
+++ boxes/ncn-common/provisioners/common/csm/cloud-init.sh
@@ -13,7 +13,6 @@ function cloud {
     mkdir -pv $base || echo "$base already exists"
     cp -pv /srv/cray/resources/common/cloud.cfg ${base}/
     rsync -av --delete /srv/cray/resources/common/cloud.cfg.d/ ${base}/cloud.cfg.d/ || echo 'No cloud-init configs to copy.'
-    rsync -av /srv/cray/resources/common/cloud/src/ /usr/lib/python3.6/site-packages/cloudinit/config/ || echo 'No cloud-init configs to copy.'
     rsync -av /srv/cray/resources/common/cloud/templates/ ${base}/templates/ || echo 'No templates to copy.'
 
     # Enable cloud-init at boot
