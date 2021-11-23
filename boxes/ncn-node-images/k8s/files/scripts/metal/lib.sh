b21d6197de61e4270c9aa455d265bffc61357552
Brad Klein <bklein@cray.com>
2021-11-23 11:39:52 -0700

    Add wait for s3 bucket creation before trying to mount s3fs partition

--- boxes/ncn-node-images/k8s/files/scripts/metal/lib.sh
+++ boxes/ncn-node-images/k8s/files/scripts/metal/lib.sh
@@ -161,6 +161,12 @@ function configure-s3fs-directory() {
     echo "Waiting for storage node to create ${s3_user}-s3-credentials secret..."
   done
 
+  until /opt/cray/platform-utils/s3/list-objects.py --bucket-name ${s3_bucket} > /dev/null 2>&1
+  do
+    sleep 5
+    echo "Waiting for storage node to create ${s3_bucket} bucket..."
+  done
+
   access_key=$(kubectl get secret ${s3_user}-s3-credentials -o json | jq -r '.data.access_key' | base64 -d)
   secret_key=$(kubectl get secret ${s3_user}-s3-credentials -o json | jq -r '.data.secret_key' | base64 -d)
   s3_endpoint=$(kubectl get secret ${s3_user}-s3-credentials -o json | jq -r '.data.http_s3_endpoint' | base64 -d)
