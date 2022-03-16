fad5ed7b7da71b498b38f15a62468039a42d93d1
cdelatte-hpe <77167617+cdelatte-hpe@users.noreply.github.com>
2022-03-16 16:17:54 -0400

    Fixup preload images for new podman format

--- boxes/ncn-node-images/storage-ceph/files/scripts/common/pre-load-images.sh
+++ boxes/ncn-node-images/storage-ceph/files/scripts/common/pre-load-images.sh
@@ -10,15 +10,15 @@ echo "Pre-loading local images"
 #  podman image load "registry.local/$name:$tag" -i $image_path$image_file
 # done
 
-podman image load registry.local/ceph/ceph:v15.2.8 -i /srv/cray/resources/common/images/ceph_v15.2.8.tar
-podman image load registry.local/ceph/ceph:v15.2.15 -i /srv/cray/resources/common/images/ceph_v15.2.15.tar
+podman image load -i /srv/cray/resources/common/images/ceph_v15.2.8.tar
+podman image load -i /srv/cray/resources/common/images/ceph_v15.2.15.tar
 podman tag  registry.local/ceph/ceph:v15.2.15 registry.local/artifactory.algol60.net/csm-docker/stable/quay.io/ceph/ceph:v15.2.15
-podman image load registry.local/ceph/ceph-grafana:6.6.2 -i /srv/cray/resources/common/images/ceph-grafana_6.6.2.tar
-podman image load registry.local/ceph/ceph-grafana:6.7.4 -i /srv/cray/resources/common/images/ceph-grafana_6.7.4.tar
-podman image load registry.local/quay.io/prometheus/prometheus:v2.18.1 -i /srv/cray/resources/common/images/prometheus_v2.18.1.tar
-podman image load registry.local/quay.io/prometheus/alertmanager:v0.20.0 -i /srv/cray/resources/common/images/alertmanager_v0.20.0.tar
-podman image load registry.local/quay.io/prometheus/alertmanager:v0.21.0 -i /srv/cray/resources/common/images/alertmanager_v0.21.0.tar
-podman image load registry.local/quay.io/prometheus/node-exporter:v0.18.1 -i /srv/cray/resources/common/images/node-exporter_v0.18.1.tar
-podman image load registry.local/quay.io/prometheus/node-exporter:v1.2.2 -i /srv/cray/resources/common/images/node-exporter_v1.2.2.tar
+podman image load -i /srv/cray/resources/common/images/ceph-grafana_6.6.2.tar
+podman image load -i /srv/cray/resources/common/images/ceph-grafana_6.7.4.tar
+podman image load -i /srv/cray/resources/common/images/prometheus_v2.18.1.tar
+podman image load -i /srv/cray/resources/common/images/alertmanager_v0.20.0.tar
+podman image load -i /srv/cray/resources/common/images/alertmanager_v0.21.0.tar
+podman image load -i /srv/cray/resources/common/images/node-exporter_v0.18.1.tar
+podman image load -i /srv/cray/resources/common/images/node-exporter_v1.2.2.tar
 
 echo "Images loaded"
