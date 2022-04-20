fbfddac9a336fa6b76906e99f2ed50960e454959
Jacob Salmela <jacob.salmela@hpe.com>
2022-04-20 13:32:41 -0500

    CASMTRIAGE-3193 fix kdump not working
    Signed-off-by: Jacob Salmela <jacob.salmela@hpe.com>

--- boxes/ncn-common/files/scripts/common/create-kdump-artifacts.sh
+++ boxes/ncn-common/files/scripts/common/create-kdump-artifacts.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-# Copyright 2021 HPED LP
+#
+# MIT License
+#
+# (C) Copyright 2021-2022 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
 # create-kdump-artifacts.sh creates an initrd for use with kdump
 #   this specialized initrd is booted when a node crashes
 #   it is specifically designed to work with the persistent overlay and RAIDs in use in Shasta 1.4+
@@ -225,6 +247,13 @@ sed -i 's/^\(ROOTDIR\)=.*$/\1=\"\/kdump\/overlay\"/' lib/kdump/save_dump.sh
 sed -i 's/^\(KDUMP_SAVEDIR\)=.*$/\1=\"file:\/\/\/var\/crash\"/' etc/sysconfig/kdump
 # optionally, uncomment to stay in the initrd after the dump is complete
 #sed -i 's/^\(KDUMP_IMMEDIATE_REBOOT\)=.*$/\1=\"no"/' etc/sysconfig/kdump
+
+# this is a hacky workaround to remove a rogue fstab entry in 1.2+
+# /kdump/mnt0 should be the ROOTRAID, not SQFSRAID
+# it is added by the kdump dracut module, but can be inaccurate if the root label matches the squashfs label
+# prior to this commit, earlier versions of csm will hit this issue,
+# so this can help provide backwards compatibility if kdump needs to be enabled on those environments
+sed -i '/^LABEL=SQFSRAID \/kdump\/mnt0/d' etc/fstab
 
 # Use a here doc to create a simple pre-script that mounts the overlay
 cat << EOF > sbin/mount_kdump_overlay.sh
