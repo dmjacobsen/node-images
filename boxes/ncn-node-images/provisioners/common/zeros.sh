d766976a3fce763b048fde33a26b1d2da3216988
Russell Bunch <doomslayer@hpe.com>
2022-05-17 14:53:59 -0500

    Squashed 'vendor/github.com/Cray-HPE/csm-rpms/' changes from 8b24a3222..695c2954a
    695c2954a MTL-1530 build a signed ilorest from the new fork
    4a11a35ea CASMINST-4600: add lsscsi to NCNs
    a187cd088 MTL-1741 Fix pattern matching for initrd check
    05479ed95 MTL-1476 Fix Builds
    2399e1477 MTL-1688-and-MTL-1687
    d33b73662 MTL-1497 New pit-init
    82c1046b2 CAST-29864 - Update dns_records.py API call to use the correct API gateway.
    553ecb6da CASMNET-1539 - Update dns_records.py API call to use the correct API gateway.
    4893a6823 license
    73c046b52 adds shellcheck to this repo. Since the warning and errors are widespread, I went though each and disabled them within the code, since changing it could change the current behavior. This gives us a clean starting point for new shell scripts that enter this repo. code owners can come back at a later time to remove the disabled checks and adjust their scripts.
    9aa70bad5 MTL-1497 add shellcheck
    34f0a2a21 MTL-1319
    aa89f02f2 MTL-1476 add unzip, xorriso, and mkisofs
    
    git-subtree-dir: vendor/github.com/Cray-HPE/csm-rpms
    git-subtree-split: 695c2954a4a2a71f13f7a891d0db7f72424284e7

--- boxes/ncn-node-images/provisioners/common/zeros.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Running defrag -- this will take a while"
-e4defrag / > /dev/null 2>&1
-echo "Write zeros..."
-filler="$(($(df -BM --output=avail /|grep -v Avail|cut -d "M" -f1)-1024))"
-dd if=/dev/zero of=/root/zero-file bs=1M count=$filler
-rm /root/zero-file
--- boxes/ncn-node-images/storage-ceph/files/resources/common/ansible/ceph-rgw-users/README.md
+++
@@ -1,2 +0,0 @@
-# This repo is for provisioning ceph radosgw users buckets and policies
-## This is not a performant deployment and is to be used only for functional testing
