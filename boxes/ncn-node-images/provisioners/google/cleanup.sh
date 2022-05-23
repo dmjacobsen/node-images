a0733ea0699d797175f188c7fa58186dce1060f5
Russell Bunch <doomslayer@hpe.com>
2022-05-23 14:03:17 -0500

    Squashed 'vendor/github.com/Cray-HPE/csm-rpms/' changes from 4f9b3c462..7d0be2006
    7d0be2006 Update SDU RPM version for NCN image.
    7f13ee833 Update base.packages
    4549d232f Update base.packages
    
    git-subtree-dir: vendor/github.com/Cray-HPE/csm-rpms
    git-subtree-split: 7d0be20066704e8fbe28d9cef43b0b4d7e05ef24

--- boxes/ncn-node-images/provisioners/google/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-echo "Modifying DNS to use Google DNS servers..."
-mv /etc/sysconfig/network/config.backup /etc/sysconfig/network/config
--- boxes/pit-common/provisioners/pit/pit-grub.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -e '/^\s*GRUB_CMDLINE_LINUX_DEFAULT=/s/="[^"]*"/="splash=silent mediacheck=0 biosdevname=1 console=tty0 console=ttyS0,115200 mitigations=auto iommu=pt pcie_ports=native transparent_hugepage=never rd.shell rd.md=0 rd.md.conf=0"/' /etc/default/grub
-grub2-mkconfig -o /boot/grub2/grub.cfg
