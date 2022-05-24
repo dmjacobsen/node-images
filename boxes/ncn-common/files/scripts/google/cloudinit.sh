e0945be05ac03c91da5700b96de0aa2b5894663d
Eric Lund <eric.lund@hpe.com>
2022-05-24 13:27:14 -0500

    VSHA-399 for vshasta get SSH keys from gcp platform metadata

--- boxes/ncn-common/files/scripts/google/cloudinit.sh
+++ boxes/ncn-common/files/scripts/google/cloudinit.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+echo "Setting up root SSH keys from 'cloud-init' (aka platform metadata)"
+craysys metadata get root-private-key > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+craysys metadata get root-public-key > /root/.ssh/id_rsa.pub
+chmod 644 /root/.ssh/id_rsa.pub
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+chmod 644 /root/.ssh/authorized_keys
+
 echo "Scheduling local DNS server(s) awareness updates for every 5 minutes"
 echo "*/5 * * * * root . /etc/profile.d/cray.sh; /etc/ansible/gcp/bin/python3 /srv/cray/scripts/google/update-dns.py >> /var/log/cray/cron.log 2>&1" > /etc/cron.d/cray-update-dns
 
